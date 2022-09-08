b8dfb30345c49c8ce51041713270275d07a5b6d8
Allen Cao <allen.m.cao@gmail.com>
2022-09-07 23:53:30 -0700

    Cleaned up rig.py to functioning class.

--- rig-restarter/rig.py
+++ rig-restarter/rig.py
@@ -1,25 +1,38 @@
+import exceptions
+
 class Rig():
     def __init__(self, rig_json):
         self.status_api = rig_json['status_api']
         self.coin = rig_json['coin']
-        self.endpoint = 'endpoint'
-        self.wallet = 'wallet'
-        self.kasa_device_ip = "kasa_device_ip"
-        self.worker_name = 'worker_name'
-        self.smart_strip_plug_name = 'smart_strip_plug_name'
-        self.smart_strip_plug_number = 'smart_strip_plug_number'
+        self.endpoint = rig_json['endpoint']
+        self.wallet = rig_json['wallet']
+        self.kasa_device_ip = rig_json["kasa_device_ip"]
+        self.worker_name = rig_json['worker_name']
+        self.smart_strip_plug_name = rig_json['smart_strip_plug_name']
+        self.smart_strip_plug_number = rig_json['smart_strip_plug_number']
         
-        self.power_cycle_on_delay = 'power_cycle_on_delay'
-        self.time_until_offline = 'time_until_offline'
-        self.status_check_frequency = 'status_check_frequency'
-        self.status_check_cooldown = 'status_check_cooldown'
-        self.max_consecutive_restarts = 'max_consecutive_restarts'
-        self.current_consecutive_restarts = 'current_consecutive_restarts'
+        self.power_cycle_on_delay = rig_json['power_cycle_on_delay']
+        self.time_until_offline = rig_json['time_until_offline']
+        self.status_check_frequency = rig_json['status_check_frequency']
+        self.status_check_cooldown = rig_json['status_check_cooldown']
+        self.max_consecutive_restarts = rig_json['max_consecutive_restarts']
+        self.current_consecutive_restarts = rig_json['current_consecutive_restarts']
     
     def has_mandatory_fields(self):
         mandatory_fields = [
-            status_api,
-            wallet, 
-            kasa_device_ip, 
-            worker_name, 
+            self.status_api,
+            self.wallet, 
+            self.kasa_device_ip, 
+            self.worker_name, 
         ]
+
+        for mand in mandatory_fields:
+            if not self.mand:
+                raise exceptions.RRMissingFieldException('Rig', mand)
+
+    def to_json(self):
+        return self.__str__()
+    
+    @staticmethod
+    def from_json(json_dct):
+      return Rig(json_dct)
